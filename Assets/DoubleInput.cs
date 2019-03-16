e6e04db7767ef595a2617f79dd7331d98d158f77
SeverinM <severinmichaut@gmail.com>
2019-03-16 14:14:02 +0100

    Test

---
+++ Assets/DoubleInput.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoubleInput : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Rotate(Vector3.up, 10);
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Rotate(Vector3.up, -10);
+        }
+    }
+}
