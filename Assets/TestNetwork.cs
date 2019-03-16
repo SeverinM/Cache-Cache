e85983c307e6dbb01b073465c7e837491220ba0a
SeverinM <severinmichaut@gmail.com>
2019-03-16 11:04:27 +0100

    test reseaux

---
+++ Assets/TestNetwork.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+public class TestNetwork : NetworkBehaviour
+{
+    private void Update()
+    {
+        if (!isLocalPlayer)
+        {
+            return;
+        }
+        else
+        {
+            if(Input.GetKey(KeyCode.A))
+            {
+                transform.localScale *= 0.9f;
+            }
+        }
+    }
+}
