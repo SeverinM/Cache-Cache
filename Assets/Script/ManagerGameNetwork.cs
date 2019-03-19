27824d92b108d313ac8420218e310d5e2c9d0e5a
SeverinM <severinmichaut@gmail.com>
2019-03-19 22:27:41 +0100

    Visibilité differente

---
+++ Assets/Script/ManagerGameNetwork.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Mirror;
+
+public class ManagerGameNetwork : NetworkManager
+{
+    [SerializeField]
+    List<GameObject> allPlayersGameObject;
+
+    int nbPlayer = 0;
+
+    public override void Awake()
+    {
+        foreach (GameObject gob in allPlayersGameObject)
+        {
+            ClientScene.RegisterPrefab(gob);
+        }
+    }
+
+    public override void OnServerAddPlayer(NetworkConnection conn, AddPlayerMessage msg)
+    {
+        GameObject gob = Instantiate(allPlayersGameObject[nbPlayer]);
+        gob.transform.position += Random.onUnitSphere * 2;
+        NetworkServer.AddPlayerForConnection(conn, gob);
+        nbPlayer++;
+        gob.GetComponent<NetworkIdentity>().AssignClientAuthority(conn);
+    }
+
+    public override void OnServerRemovePlayer(NetworkConnection conn, NetworkIdentity player)
+    {
+        base.OnServerRemovePlayer(conn, player);
+        nbPlayer--;
+    }
+}
---
+++ Assets/Script/TestPlayer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+public class TestPlayer : NetworkBehaviour
+{
+    private void Update()
+    {
+        if (isLocalPlayer && Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(Vector3.up, 10);
+        }
+    }
+
+    public override bool OnCheckObserver(NetworkConnection conn)
+    {
+        Debug.Log("kop");
+        if (!hasAuthority)
+            return false;
+        return true;
+    }
+}
