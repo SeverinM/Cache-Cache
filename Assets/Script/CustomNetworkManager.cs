38c1c00994aa18371e8dfec7dddd0b4c3f0f4b83
SeverinM <severinmichaut@gmail.com>
2019-03-21 19:58:34 +0100

    Test

--- Assets/Script/CustomNetworkManager.cs
+++ Assets/Script/CustomNetworkManager.cs
@@ -2,15 +2,39 @@
 using System.Collections.Generic;
 using UnityEngine;
 using Mirror;
+using System.Linq;
 
 public class CustomNetworkManager : NetworkManager
 {
+    int nbPlayer = 0;
+    public GameObject prefab1;
+    public GameObject prefab2;
+
     public override void OnServerAddPlayer(NetworkConnection conn, AddPlayerMessage extraMessage)
     {
-        base.OnServerAddPlayer(conn, extraMessage);
+        GameObject player = Instantiate(playerPrefab, GetStartPosition().transform.position,Quaternion.identity);
+        NetworkServer.AddPlayerForConnection(conn, player);
+
         foreach(Player plyr in GameObject.FindObjectsOfType<Player>())
         {
             plyr.RpcUpdateCam();
+        }
+
+        player.GetComponent<Player>().RpcSetMaquette(nbPlayer == 0 ? prefab1 : prefab2);
+        nbPlayer++;
+
+        if (nbPlayer == 2)
+        {
+
+            List<Player> allGobs = GameObject.FindObjectsOfType<Player>().OrderBy(x => x.id).ToList();
+
+            GameObject gob = Instantiate(prefab1);
+            gob.transform.position = allGobs[0].transform.position + new Vector3(0, 20, 0);
+            NetworkServer.Spawn(gob);
+
+            GameObject gob2 = Instantiate(prefab2);
+            gob2.transform.position = allGobs[1].transform.position + new Vector3(0, 20, 0);
+            NetworkServer.Spawn(gob2);
         }
     }
 }
--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -7,6 +7,14 @@ using Mirror;
 public class Player : NetworkBehaviour
 {
     public static int count = 0;
+    public int id;
+
+    void Awake()
+    {
+        id = count;
+        count++;
+    }
+
 
     [ClientRpc]
     public void RpcUpdateCam()
@@ -24,8 +32,18 @@ public class Player : NetworkBehaviour
                     cam.enabled = true;
                 }
             }
+        }
+    }
 
-            transform.LookAt(new Vector3(0, 0, 0));
+    [ClientRpc]
+    public void RpcSetMaquette(GameObject maquette)
+    {
+        if (hasAuthority)
+        {
+            Debug.Log(maquette);
+            maquette.transform.position = transform.position + (Random.onUnitSphere * 3);
+            transform.LookAt(maquette.transform.position);
+            transform.Rotate(Vector3.right, 90);
         }
     }
 }
