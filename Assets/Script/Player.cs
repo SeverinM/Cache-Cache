e43da48de78825a92f51fdf3a84ad40cd1e2a1eb
SeverinM <severinmichaut@gmail.com>
2019-03-21 13:28:09 +0100

    Table rase

---
+++ Assets/Script/Player.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Mirror;
+
+public class Player : NetworkBehaviour
+{
+    public static int count = 0;
+
+    public void Start()
+    {
+        if (isLocalPlayer)
+        {
+            Utils.DisableAllOthersCam(GetComponent<Camera>());
+            transform.LookAt(new Vector3(0, 0, 0));
+            count++;
+            GameObject.FindObjectOfType<Text>().text = count.ToString();
+        }       
+    }
+}
---
+++ Assets/Script/Utils.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Utils : MonoBehaviour
+{
+    public static void DisableAllOthersCam(Camera cam)
+    {
+        foreach(Camera camera in GameObject.FindObjectsOfType<Camera>())
+        {
+            if (cam != camera)
+            {
+                camera.enabled = false;
+            }
+        }
+        cam.enabled = true;
+    }
+}
