fb5431ef05f6e94b17a6905f74591f1e6cd43212
SeverinM <severinmichaut@gmail.com>
2019-04-18 00:52:30 +0200

    Refonte drag and drop

---
+++ Assets/Script/Spots/Classic.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Classic : Spot
+{
+    public override void CursorEnter()
+    {
+        Debug.Log("enter");
+    }
+
+    public override void CursorExit()
+    {
+        Debug.Log("exit");
+    }
+
+    public override void CursorRelease()
+    {
+        Debug.Log("release");
+    }
+}
---
+++ Assets/Script/Spots/Spot.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Spot : MonoBehaviour
+{
+    //Le drag rentre dans le spot
+    public abstract void CursorEnter();
+
+    //Le drag quitte le spot
+    public abstract void CursorExit();
+
+    //Le drag prend fin
+    public abstract void CursorRelease();
+
+}
