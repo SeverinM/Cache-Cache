9e09756f4ea56a10b68bf2edc500014b7182eb55
RIBOUT <pifi77@hotmail.fr>
2019-05-01 20:25:45 +0200

    rotation du fond

---
+++ Assets/Script/HoraceSamuse/AutoRot.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoRot : MonoBehaviour
+{
+    public Vector3 rotationVec = Vector3.up;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(rotationVec * Time.deltaTime, Space.World);
+    }
+}
