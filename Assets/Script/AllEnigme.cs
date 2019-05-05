4e2a1789347857c5ea58c413c53d7f25b2190d1e
SeverinM <severinmichaut@gmail.com>
2019-05-05 16:03:22 +0200

    Quelques oublis

--- Assets/Script/AllEnigme.cs
+++ Assets/Script/AllEnigme.cs
@@ -4,8 +4,18 @@ using UnityEngine;
 
 public static class AllEnigme 
 {
-    public static bool IS_MOON_OPEN(MoonPart part1 , MoonPart part2)
+    public static bool IS_MOON_OPEN(params Interactable[] allInter)
     {
-        return (part1.Progress == 0 && part2.Progress == 0 && part1.Ratio >= 0.999f && part2.Ratio >= 0.999f);
+        MoonPart moon1 = (MoonPart)allInter[0];
+        MoonPart moon2 = (MoonPart)allInter[1];
+        if (!moon1 || !moon2)
+            return false;
+
+        return (allInter[0].Progress == 0 && allInter[1].Progress == 0 && moon1.Ratio >= 0.999f && moon2.Ratio >= 0.999f);
+    }
+
+    public static bool IS_BOX_OPEN(params Interactable[] allInter)
+    {
+        return false;
     }
 }
