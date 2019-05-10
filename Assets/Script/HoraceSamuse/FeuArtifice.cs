904ff4a50c7e5f8f04f5210b7057c650a737e777
RIBOUT <pifi77@hotmail.fr>
2019-05-10 11:16:15 +0200

    Particle : neige et feu artifice

---
+++ Assets/Script/HoraceSamuse/FeuArtifice.cs
@@ -0,0 +1,138 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FeuArtifice : MonoBehaviour
+{
+    [System.Serializable]
+    public class SousArtifice
+    {
+        public Transform tr;
+        public Gradient colorGradient;
+        public SpriteRenderer sP;
+        public AnimationCurve scaleCurve;
+
+        public float randomGravity = 1;
+        public float randomSpeed = 1;
+
+        public TrailRenderer trail;
+    }
+
+    [Header("Ascending")]
+    public SpriteRenderer sP;
+    public float ascendingDuration;
+    public float ascendingSpeed;
+    public Vector2 minMaxDecal = new Vector2(-0.1f, 0.1f);
+    public Gradient ascendingColor;
+    public AnimationCurve ascendingCurve;
+    public AnimationCurve lateralCurve;
+    public AnimationCurve scaleCurve;
+    public TrailRenderer tR;
+
+    [Header("Wait")]
+    public float waitBeforeSplosion;
+
+    [Header("Explosion")]
+    public GameObject prefabSousArtifice;
+    public Vector2 minMaxBranches = new Vector2(4, 8);
+    public AnimationCurve sousArtificeScaleCurve;
+
+    [Header("Branches")]
+    public float brancheDuration;
+    public float brancheSpeed;
+    public Gradient colorGradient;
+    public AnimationCurve gravitationCurve;
+    public Vector2 randGraviMinMax = new Vector2(0.95f, 1.05f);
+    public Vector2 randSpeedMinMax = new Vector2(0.8f, 1.2f);
+
+    [SerializeField]
+    public List<SousArtifice> branches = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (sP == null)
+            sP = this.GetComponent<SpriteRenderer>();
+        if (tR == null)
+            tR = this.GetComponent<TrailRenderer>();
+        StartCoroutine(Ascending());
+    }
+
+    IEnumerator Ascending()
+    {
+        float lerpVal = 0;
+        Vector3 directionLeftRight = new Vector3(Random.Range(minMaxDecal.x, minMaxDecal.y), 0, Random.Range(minMaxDecal.x, minMaxDecal.y));
+        Vector3 directionUp = new Vector3(0, ascendingSpeed, 0);
+        while (lerpVal < 1)
+        {
+            this.transform.position += directionUp * ascendingCurve.Evaluate(lerpVal) + directionLeftRight * lateralCurve.Evaluate(lerpVal);
+            this.transform.localScale = Vector3.one * scaleCurve.Evaluate(lerpVal);
+            tR.startWidth = transform.localScale.y * 0.7f;
+
+            sP.color = ascendingColor.Evaluate(lerpVal);
+            tR.startColor = sP.color;
+
+            lerpVal += Time.deltaTime / ascendingDuration;
+
+
+            yield return new WaitForSeconds(0.01f);
+        }
+        Debug.Log("ReachIt !");
+        sP.enabled = false;
+        yield return new WaitForSeconds(waitBeforeSplosion);
+        this.transform.localScale = Vector3.one;
+        Explosion();
+    } 
+
+    public void Explosion()
+    {
+        Debug.Log("Explosion !");
+        branches = new List<SousArtifice>();
+        int nmbrBranch = Random.Range((int)minMaxBranches.x, (int)minMaxBranches.y);
+        for (int i = 0; i < nmbrBranch; i++)
+        {
+            Quaternion rot = this.transform.rotation;
+            GameObject g0 = Instantiate(prefabSousArtifice, this.transform.position, rot, this.transform);
+            SousArtifice sA = new SousArtifice();
+            sA.tr = g0.transform;
+            sA.sP = g0.GetComponent<SpriteRenderer>();
+            sA.trail = g0.GetComponent<TrailRenderer>();
+            sA.colorGradient = colorGradient;
+            sA.scaleCurve = sousArtificeScaleCurve;
+            branches.Add(sA);
+
+            sA.tr.Rotate(0, 0, i * (float)360 / (float)nmbrBranch);
+
+            sA.randomGravity = Random.Range(randGraviMinMax.x, randGraviMinMax.y);
+            sA.randomSpeed = Random.Range(randSpeedMinMax.x, randSpeedMinMax.y);
+        }
+        this.transform.Rotate(0, 0, Random.Range(0, 359));
+        StartCoroutine(ExplosionBranches());
+    }
+
+    IEnumerator ExplosionBranches()
+    {
+        float lerpVal = 0;
+        Vector3 directionBase = new Vector3(0, brancheSpeed, 0);
+        while (lerpVal < 1)
+        {
+            foreach (SousArtifice sA in branches)
+            {
+                sA.tr.Translate(directionBase * sA.scaleCurve.Evaluate(lerpVal) * sA.randomSpeed, Space.Self);
+                sA.tr.position += Vector3.down * gravitationCurve.Evaluate(lerpVal) * sA.randomGravity;
+                sA.tr.localScale = Vector3.one * sA.scaleCurve.Evaluate(Mathf.Clamp01(lerpVal * sA.randomSpeed));
+                sA.trail.startWidth = sA.tr.localScale.y * 0.7f;
+                sA.sP.color = sA.colorGradient.Evaluate(lerpVal);
+                sA.trail.startColor = sA.sP.color;
+            }
+            lerpVal += Time.deltaTime / brancheDuration;
+            yield return new WaitForSeconds(0.01f);
+        }
+        Debug.Log("FinishBlowing");
+        foreach (SousArtifice sA in branches)
+        {
+            Destroy(sA.tr.gameObject);
+        }
+        Destroy(this.gameObject);
+    }
+}
