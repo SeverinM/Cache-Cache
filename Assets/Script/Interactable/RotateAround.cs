a9ed065b1fd6129ca7ae65d221f2572962c226a7
SeverinM <severinmichaut@gmail.com>
2019-05-17 16:10:55 +0200

    [A TESTER] deuxieme edition

--- Assets/Script/Interactable/RotateAround.cs
+++ Assets/Script/Interactable/RotateAround.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using System.Linq;
 
 public class RotateAround : Interactable
 {
@@ -17,16 +18,27 @@ public class RotateAround : Interactable
     [SerializeField]
     float speed;
     float currentSpeed = 0;
+    public float CurrentSpeed => currentSpeed;
+
+    static float GlobalSpeed => GameObject.FindObjectsOfType<RotateAround>().Sum(x => x.CurrentSpeed);
+    bool isDown = false;
 
     public override void MouseDown(MouseInputManager.MouseButton btn, MouseInputManager.MousePointer mouse, Interactable echo = null)
     {
-        if (!btn.Equals(MouseInputManager.MouseButton.LEFT_BUTTON) || (!GetComponent<Button>().interactable && !echo)) return;
-        currentSpeed += speed;
-
+        if (!btn.Equals(MouseInputManager.MouseButton.LEFT_BUTTON)) return;
+       
         if (echo)
         {
             GetComponent<Button>().interactable = false;
             otherButton.interactable = false;
+        }
+        else
+        {
+            if (GlobalSpeed == 0)
+            {
+                currentSpeed += speed;
+                isDown = true;
+            }           
         }
     }
 
@@ -44,12 +56,20 @@ public class RotateAround : Interactable
 
     public override void MouseUp(MouseInputManager.MouseButton btn, MouseInputManager.MousePointer mouse, Interactable echo = null)
     {
-        if (!btn.Equals(MouseInputManager.MouseButton.LEFT_BUTTON) || (!GetComponent<Button>().interactable && !echo)) return;
-        currentSpeed -= speed;
+        if (!btn.Equals(MouseInputManager.MouseButton.LEFT_BUTTON)) return;
+        
         if (echo)
         {
             GetComponent<Button>().interactable = true;
             otherButton.interactable = true;
+        }
+        else
+        {
+            if (CurrentSpeed != 0 && isDown)
+            {
+                currentSpeed = 0;
+                isDown = false;
+            }
         }
     }
 
