15041dd654975a14a1d24b3217d1b43a99b46202
SeverinM <severinmichaut@gmail.com>
2019-05-17 16:44:56 +0200

    Test 5

--- Assets/Script/Interactable/RotateAround.cs
+++ Assets/Script/Interactable/RotateAround.cs
@@ -14,6 +14,7 @@ public class RotateAround : Interactable
 
     [SerializeField]
     Button otherButton;
+    public Button OtherButton => otherButton;
 
     [SerializeField]
     float speed;
@@ -36,20 +37,16 @@ public class RotateAround : Interactable
     public override void MouseDown(MouseInputManager.MouseButton btn, MouseInputManager.MousePointer mouse, Interactable echo = null)
     {
         if (!btn.Equals(MouseInputManager.MouseButton.LEFT_BUTTON)) return;
+        if (echo) return;
 
-        if(!echo && !holdBy)
+        if (!holdBy)
         {
             holdBy = this;
             currentSpeed += speed;
+            (Echo as RotateAround).currentSpeed += speed;
+            Echo.GetComponent<Button>().interactable = false;
+            (Echo as RotateAround).OtherButton.interactable = false;
         }
-
-        if (echo && holdBy)
-        {
-            GetComponent<Button>().interactable = false;
-            otherButton.interactable = false;
-            currentSpeed += speed;
-        }
-            
     }
 
     public override void MouseEnter(MouseInputManager.MouseButton btn, MouseInputManager.MousePointer mouse, Interactable echo = null)
@@ -67,18 +64,15 @@ public class RotateAround : Interactable
     public override void MouseUp(MouseInputManager.MouseButton btn, MouseInputManager.MousePointer mouse, Interactable echo = null)
     {
         if (!btn.Equals(MouseInputManager.MouseButton.LEFT_BUTTON)) return;
+        if (echo) return;
 
-        if (!echo && holdBy == this)
+        if (holdBy == this)
         {
             holdBy = null;
             currentSpeed = 0;
-        }
-        
-        if (echo && !holdBy)
-        {
-            currentSpeed = 0;
-            GetComponent<Button>().interactable = true;
-            otherButton.interactable = true;
+            (Echo as RotateAround).currentSpeed = 0;
+            Echo.GetComponent<Button>().interactable = true;
+            (Echo as RotateAround).OtherButton.interactable = true;
         }
     }
 
