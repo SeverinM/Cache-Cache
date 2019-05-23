b5357b7c42495858a9baf9866d9690b74b5ff63c
Hugo Dunas-Wald <alterisateur@gmail.com>
2019-05-23 12:20:41 +0200

    Le bouton fait tourner la roue

---
+++ Assets/Script/Grande Roue/BoutonGrandeRoue.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoutonGrandeRoue : Interactable
+{
+    public bool turnTheWheel = false;
+
+    bool isOnButton = false;
+    bool isPressingButton = false;
+
+    public override void MouseDown(MouseInputManager.MouseButton btn, MouseInputManager.MousePointer mouse, Interactable echo = null)
+    {
+        isPressingButton = true;
+    }
+
+    public override void MouseEnter(MouseInputManager.MouseButton btn, MouseInputManager.MousePointer mouse, Interactable echo = null)
+    {
+        isOnButton = true;
+    }
+
+    public override void MouseLeave(MouseInputManager.MouseButton btn, MouseInputManager.MousePointer mouse, Interactable echo = null)
+    {
+        isOnButton = false;
+    }
+
+    public override void MouseMove(MouseInputManager.MouseButton btn, MouseInputManager.MousePointer mouse, Interactable echo = null)
+    {
+        
+    }
+
+    public override void MouseUp(MouseInputManager.MouseButton btn, MouseInputManager.MousePointer mouse, Interactable echo = null)
+    {
+        isPressingButton = false;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(isOnButton && isPressingButton)
+        {
+            turnTheWheel = true;
+        }
+        else
+        {
+            turnTheWheel = false;
+        }
+    }
+}
---
+++ Assets/Script/Grande Roue/GrandeRoue.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrandeRoue : MonoBehaviour
+{
+    [SerializeField]
+    BoutonGrandeRoue Bouton;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Bouton.turnTheWheel)
+        {
+            this.transform.Rotate(1, 0, 0);
+        }
+    }
+}
