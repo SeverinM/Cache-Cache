3404f3e2d90b7304f82fe62f0d0ddd43299d874f
RIBOUT <pifi77@hotmail.fr>
2019-05-26 19:16:36 +0200

    some weed (de l'herbe)

---
+++ Assets/Script/HoraceSamuse/Herbe.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Herbe : MonoBehaviour
+{
+    public RotateAround rot1;
+    public RotateAround rot2;
+
+    public Material myMat;
+
+    private bool goBackToZero = false;
+
+    public void Start()
+    {
+        if (myMat == null)
+            myMat = this.GetComponent<MeshRenderer>().material;
+    }
+
+    public void Update()
+    {
+        if(rot1.CurrentSpeed != 0)
+        {
+            SetUpSpeed(rot1);
+        }
+        else if(rot2.CurrentSpeed != 0)
+        {
+            SetUpSpeed(rot2);
+        }
+        else if (goBackToZero)
+        {
+            SetUpZero();
+            goBackToZero = false;
+        }
+    }
+
+    public void SetUpSpeed(RotateAround rot)
+    {
+        myMat.SetFloat("_RotationSpeed", -rot.CurrentSpeed * 10);
+
+        goBackToZero = true;
+    }
+
+    public void SetUpZero()
+    {
+        myMat.SetFloat("_RotationSpeed", 0);
+    }
+
+}
