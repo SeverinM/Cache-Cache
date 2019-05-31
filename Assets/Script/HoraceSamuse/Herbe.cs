a48813858c8fcba34101c7e0bc5da75ee03a1c6c
Hugo Dunas-Wald <alterisateur@gmail.com>
2019-05-31 10:50:25 +0200

    Correction herber + ajout hitbox sur le feur de la maison
    bug : collider maison est toujours accessible quand la maison est fermée

--- Assets/Script/HoraceSamuse/Herbe.cs
+++ Assets/Script/HoraceSamuse/Herbe.cs
@@ -25,7 +25,7 @@ public class Herbe : MonoBehaviour
         if (myMat == null)
             myMat = this.GetComponent<MeshRenderer>().material;
 
-        myMat.SetVector("_SourceCenter", center.localPosition);
+        myMat.SetVector("_SourceCenter", center.position);
     }
 
     public void Update()
