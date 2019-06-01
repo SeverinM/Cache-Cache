6e3f917066ef31d5237ad8b215121501e5bedbc7
SeverinM <severinmichaut@gmail.com>
2019-06-01 16:11:59 +0200

    Queue de nessie

---
+++ Assets/Script/NessieTail.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NessieTail : Interactable
+{
+    bool busy = false;
+
+    [SerializeField]
+    AnimationCurve curve;
+
+    [SerializeField]
+    float duration;
+
+    public override void MouseDown(MouseInputManager.MouseButton btn, MouseInputManager.MousePointer mouse, Interactable echo = null)
+    {
+        if (!busy)
+            StartCoroutine(TailRotate());
+    }
+
+    public override void MouseEnter(MouseInputManager.MouseButton btn, MouseInputManager.MousePointer mouse, Interactable echo = null)
+    {
+    }
+
+    public override void MouseLeave(MouseInputManager.MouseButton btn, MouseInputManager.MousePointer mouse, Interactable echo = null)
+    {
+    }
+
+    public override void MouseMove(MouseInputManager.MouseButton btn, MouseInputManager.MousePointer mouse, Interactable echo = null)
+    {
+    }
+
+    public override void MouseUp(MouseInputManager.MouseButton btn, MouseInputManager.MousePointer mouse, Interactable echo = null)
+    {
+    }
+
+    IEnumerator TailRotate()
+    {
+        busy = true;
+        float normalizedTime = 0;
+        while (normalizedTime < 1)
+        {
+            normalizedTime += Time.deltaTime / duration;
+            transform.localEulerAngles = new Vector3(0, Mathf.Lerp(0, 720, curve.Evaluate(normalizedTime)));
+            yield return null;
+        }
+        busy = false;
+    }
+}
